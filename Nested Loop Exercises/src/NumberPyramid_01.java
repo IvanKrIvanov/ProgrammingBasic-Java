19dc5f14ff7883d6434fe2cb57af9b9bed9db387
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2024-01-04 18:34:52 +0200

    Nested Loop Exercises

---
+++ Nested Loop Exercises/src/NumberPyramid_01.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class NumberPyramid_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int num = 1;
+
+        for (int row = 1; row <= n; row++) {
+
+            for (int j = 1; j <= row; j++) {
+                if (num > n) {
+                    break;
+                }
+
+                System.out.print(num + " ");
+                ++num;
+            }
+
+            if (num > n) {
+                break;
+            }
+
+            System.out.println();
+        }
+    }
+}
